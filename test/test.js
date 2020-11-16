ec3a26b9e159b16a858f24feef94da645aafe46d
Aashish Kumar <kumaraashish@facileconsulting.com>
2020-11-16 12:11:05 +0530

    changes for CI and CD with github actions

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../index");
 
 describe("GET /", () => {
-    it("respond with Hello World", (done) => {
-        request(app).get("/").expect("Hello World", done);
+    it("respond with welcome to CI and CD with github actions ", (done) => {
+        request(app).get("/").expect("Welcome to Ci and CD with github actions", done);
     })
 });
